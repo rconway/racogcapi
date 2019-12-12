85d35919a8b4459489e58fd04e5096168db77e85
Richard Conway <richard@rconway.co.uk>
2019-12-12 17:48:26 +0000

    Move landing-page component into own module + tab.

--- main.js
+++ main.js
@@ -3,55 +3,16 @@ var app = new Vue({
     el: '#app',
     data: {
         title: "OGC API Test Client",
-        baseUrl: "http://localhost:3456/javaps/rest",
-        fetchStatus: null,
-        landingJson: null
-    },
-    computed: {
-        landingJsonString() {
-            return JSON.stringify(this.landingJson, null, 2)
-        }
+        // baseUrl: "http://localhost:3456/javaps/rest",
+        baseUrl: "http://geoprocessing.demo.52north.org:8080/javaps/rest",
+        landingJson: null,
+        currentTab: "landing-page",
+        tabs: ["landing-page", "fredbob"]
     },
     methods: {
-        fetchLandingPage() {
-            self = this
-            console.log("zzz CLICKED zzz")
-            self.fetchStatus = "FETCHING"
-            fetch(self.baseUrl + "/", {
-                method: "GET",
-                mode: "cors",
-                headers: {
-                    accept: "application/json",
-                }
-            })
-            // fetch(self.baseUrl + "/")
-            .then(function(response) {
-                self.fetchStatus = "RESPONSE"
-                console.log(response)
-                if (response.ok) {
-                    self.fetchStatus = "JSON"
-                    response.json()
-                    .then(data => {
-                        console.log(data)
-                        console.log(JSON.stringify(data, null, 2))
-                        self.landingJson = data
-                        self.fetchStatus += " [done]"
-                    })
-                    .catch(error => {
-                        console.error(error)
-                        self.fetchStatus += " [error]"
-                    })
-                }
-                else {
-                    console.error("bad response from 'landing page' endpoint")
-                    self.fetchStatus += " [error]"
-                }
-            })
-            .catch(error => {
-                console.error(error)
-                self.fetchStatus += " [error]"
-            })
-            console.log("ZZZ CLICKED ZZZ")
+        rxLandingJson(value) {
+            this.landingJson = value
+            console.log("[rxLandingJson] => " + JSON.stringify(this.landingJson, null, 2))
         }
     }
 })
