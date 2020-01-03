f5d4a857b4cbe1f119dee81f84ef390296838bd7
Richard Conway <richard.conway@telespazio.com>
2020-01-03 14:55:40 +0000

    Added known services helper.

--- src/components/ServiceSelector.js
+++ src/components/ServiceSelector.js
@@ -6,21 +6,42 @@ class ServiceSelector extends React.Component {
     this.state = {
       baseUrl: props.baseUrl ? props.baseUrl : ""
     };
+
+    this.knownServicesRendered = React.Children.map(
+      this.props.children,
+      child => {
+        if (child.type === "service") {
+          const { name, url } = child.props;
+          return (
+            <div key={name}>
+              <span>{name} => </span>
+              <button onClick={() => this.setBaseUrl(url)}>{url}</button>
+            </div>
+          );
+        }
+      }
+    );
   }
 
   updateBaseUrl = event => {
     this.setState({ baseUrl: event.target.value });
   };
 
+  setBaseUrl = baseUrl => {
+    this.setState({ baseUrl: baseUrl });
+    this.props.setBaseUrl(baseUrl);
+  };
+
   handleSubmit = event => {
     event.preventDefault();
-    this.props.setBaseUrl(this.state.baseUrl);
+    this.setBaseUrl(this.state.baseUrl);
   };
 
   render() {
     return (
       <div>
         ServiceSelector
+        {this.knownServicesRendered}
         <form onSubmit={this.handleSubmit}>
           <label>
             Base URL:{" "}
