4ee189296996f1b3ca40c5c4742986c42cae4de8
Richard Conway <richard.conway@telespazio.com>
2020-01-13 17:37:45 +0000

    Added bootstrap styling for TabBar.

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -3,8 +3,12 @@ import React from "react";
 class TabBar extends React.Component {
   render() {
     const tabButtons = this.props.tabs.map((tab, index) => {
+      let className =
+        this.props.selectedTab === index ? "btn btn-info" : "btn btn-secondary";
       return (
         <button
+          type="button"
+          className={className}
           key={index}
           onClick={() => this.props.setSelectedTab(index)}
         >{`${tab} (${index})`}</button>
